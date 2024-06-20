820e14aaed743cd05f69aa6bd3aee2d8c5622698
Satyam Rana <57563008+satyamranatc@users.noreply.github.com>
2024-06-20 23:07:24 +0530

    Done-2

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,12 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors());
-app.use(express.json()); // To parse JSON bodies
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.json({"Data":[1,2,3,4,5,6,7,8,10]})
 });
 
-app.post('/api/data', (req, res) => {
-  const data = req.body;
-  console.log('Received data:', data);
-  res.status(200).send({ success: true });
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
 });
